ea8b73ab638bec76a575a0f15b68b32fd5d1f30a
TrAnMu <Travis@wireless-10-105-7-241.umd.edu>
2017-04-24 02:18:22 -0400

    Mr. Presley is in (on) the building

---
+++ Elvis Vs Grannies/Assets/Big_Vegas/elvisController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elvisController : MonoBehaviour {
+	static Animator animator;
+	public float speed;
+	public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float translation = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+		float rotation = Input.GetAxis ("Horizontal") * rotationSpeed * Time.deltaTime;
+		transform.Translate (0, 0, translation);
+		transform.Rotate (0, rotation, 0);
+
+		if (translation != 0) {
+			animator.SetBool ("isJogging", true);
+		} else {
+			animator.SetBool ("isJogging", false);
+		}
+	}
+}
