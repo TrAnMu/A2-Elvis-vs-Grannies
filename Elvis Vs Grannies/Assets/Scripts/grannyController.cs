78af539a0fec8e1b6c138d7b9f662c03f7863e96
TrAnMu <Travis@wireless-10-105-7-241.umd.edu>
2017-04-26 22:28:55 -0400

    Grannies Behave Themselves

--- Elvis Vs Grannies/Assets/Scripts/grannyController.cs
+++ Elvis Vs Grannies/Assets/Scripts/grannyController.cs
@@ -4,35 +4,45 @@ using UnityEngine;
 
 public class grannyController : MonoBehaviour {
 	private Animator animator;
+	public Rigidbody rb;
 	public GameObject elvis;
-	public float speed = 2.0F;
-	public float rotationSpeed = 100.0F;
+	private float maxSpeed = 2.0F;
+	private bool falling = false;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody> ();
 		elvis = GameObject.FindWithTag ("Player");
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		/*float translation = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
-		float rotation = Input.GetAxis ("Horizontal") * rotationSpeed * Time.deltaTime;
-		transform.Translate (0, 0, translation);
-		transform.Rotate (0, rotation, 0);
+		if (falling == false && Physics.Raycast (transform.position + Vector3.up, Vector3.down)) {
+			// Chase!
+			if (Vector3.Distance (elvis.transform.position, transform.position) <= 5) {
+				animator.SetBool ("isWalking", true);
+				Vector3 grannyToElvis = elvis.transform.position - transform.position;
+				grannyToElvis.y = 0.0F;
+				grannyToElvis *= 0.01F;
+				Vector3 newVelocity = rb.velocity + grannyToElvis;
+				rb.velocity = Vector3.ClampMagnitude (newVelocity, maxSpeed);
 
-		if (translation != 0) {
-			animator.SetBool ("isWalking", true);
+				Quaternion look = new Quaternion ();
+				look.SetLookRotation (rb.velocity, Vector3.up);
+				rb.MoveRotation (look);
+			} else { // ...or Stop.
+				animator.SetBool ("isWalking", false);
+				rb.velocity = Vector3.zero;
+			}
 		} else {
-			animator.SetBool ("isWalking", false);
-		}*/
+			if (falling == false) {
+				animator.SetBool ("isFalling", true);
+				falling = true;
+				rb.constraints = RigidbodyConstraints.None;
+			}
 
-		if (Vector3.Distance (elvis.transform.position, transform.position) <= 5) {
-			//TODO GET HIM!
-		}
-
-		if (transform.position.y < -1.0F) {
-			animator.SetBool ("isFalling", true);
+			rb.velocity -= new Vector3 (0, 2.0F * Time.deltaTime, 0);
 		}
 	}
 }
